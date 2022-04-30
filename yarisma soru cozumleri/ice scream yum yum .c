58e6e6289a0114f3dd8a365e26ace3dff527e3f8
hbasriuzun <102587414+hbasriuzun@users.noreply.github.com>
2022-04-30 15:45:51 +0300

    İce scream yum-yum
    I solved the prablems.
    link is = https://www.algoleague.com/problem/ice-cream-yum-yum/detail

--- yarisma soru cozumleri/ice scream yum yum .c
+++ yarisma soru cozumleri/ice scream yum yum .c
@@ -2,5 +2,20 @@
 
 int main()
 {
-    
+    int n, num,i,typ,sum=0;
+    scanf("%d",&n);
+
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&num);
+        sum += num;
+        if (i == 0)
+            typ = num;
+        if (typ > num)
+            typ=num;
+    }
+
+    sum = sum - typ;
+    printf("%d",sum);
+    return 0;
 }
