45ef3efe155617d005acebdfaa416d6b2250a84d
hbasriuzun <102587414+hbasriuzun@users.noreply.github.com>
2022-10-11 23:48:23 +0300

    SA

---
+++ Compatitive Programming/1. input and output .cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a=123, b=543;
+    string x = "monkey";
+
+    cout << a << " " << b << " " << x << "\n";
+    printf("%d and %d",a,b);
+
+    string s;
+    getline(cin,s);
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    return 0;
+}
---
+++ Compatitive Programming/2. Working with number .cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    long long x = 123456789123456789LL;
+    
+    return 0;
+}
