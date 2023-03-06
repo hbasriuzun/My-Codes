162bf355caf04fca91d56916c3470801d58165e1
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-03-06 21:23:57 +0300

    Create slm.cpp

---
+++ slm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "The number is: " << num << endl;
+    return 0;
+}
