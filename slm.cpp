3e90a67c7afbefd39365b9a3cc20195451ec5770
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-03-11 18:44:51 +0300

    s

--- slm.cpp
+++ slm.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+double f(double x)
+{
+    double sum = x*x;
+    return sum;
+}
 int main(int argc, char const *argv[])
 {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "The number is: " << num << endl;
-    return 0;
+    double dx ,sum=0;
+    dx = 0.125;
+
+    for (int i = 0; i < 8; i++)
+    {
+        double before = f(i*dx)*dx;
+        sum += before;
+    }
+    cout << sum << endl;
 }
