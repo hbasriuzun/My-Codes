c83faf90dfcd9696ed36a2bffa540238761268ad
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-03-13 17:50:56 +0300

    compatitive programming

--- slm.cpp
+++ slm.cpp
@@ -3,20 +3,65 @@
 
 using namespace std;
 
-double f(double x)
-{
-    double sum = x*x;
-    return sum;
-}
+typedef long long ll;
+
 int main(int argc, char const *argv[])
 {
-    double dx ,sum=0;
-    dx = 0.125;
+    ll n, m, S_x, S_y;
+    cin >> n >> m >> S_x >> S_y;
+    ll max = n *m;
+    cout << S_y << " " << S_x << endl;
+    ll SS_x = S_x, SS_y = S_y;
+    
+        while (SS_x != 1)
+        {
+            SS_x--;
+            cout << S_y << " " << SS_x << endl;
+        }
+        while (S_x != m)
+        {
+            S_x++;
+            cout << S_y << " " << S_x << endl;
+        }
+        
+        while(++S_y < n)
+        {
+            while (S_x != 0)
+            {
+                cout << S_y << " " << S_x << endl;
+                S_x--;
+            }
+            if(++S_y < 5)
+                break;
+            while (++S_x < m)
+            {
+                cout << S_y << " " << S_x << endl;
 
-    for (int i = 0; i < 8; i++)
-    {
-        double before = f(i*dx)*dx;
-        sum += before;
-    }
-    cout << sum << endl;
+            }
+            cout << S_y << " " << S_x << endl;
+        }
+        S_y = 0;
+        while (S_y++ != SS_y)
+        {
+            int bomb = 0;
+            while (S_x > 0)
+            {
+                cout << S_y << " " << S_x << endl;
+                S_x--;
+                bomb++;
+            }
+            if(bomb != 0)
+                continue;
+
+            while (S_x != m)
+            {
+                S_x++;
+                cout << S_y << " " << S_x << endl;      
+            }
+        }
+        
+        
+    
+    return 0;
+    
 }
