3a871763255785ddf1bcb3eb2a968e2202679f70
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-03-22 17:56:28 +0300

    slm

--- Contest/Algorithm Program 2022-2023 Spring - Foundation Qualification Round/ODDDD.cpp
+++ Contest/Algorithm Program 2022-2023 Spring - Foundation Qualification Round/ODDDD.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    unsigned long long N = 100000000000000000; 
-   // cin >> N;
+    unsigned long long N ; 
+    cin >> N;
     unsigned long long sum = (N/2) * (N+1);
     cout << sum;
     return 0;
--- slm.cpp
+++ slm.cpp
@@ -7,61 +7,7 @@ typedef long long ll;
 
 int main(int argc, char const *argv[])
 {
-    ll n, m, S_x, S_y;
-    cin >> n >> m >> S_x >> S_y;
-    ll max = n *m;
-    cout << S_y << " " << S_x << endl;
-    ll SS_x = S_x, SS_y = S_y;
-    
-        while (SS_x != 1)
-        {
-            SS_x--;
-            cout << S_y << " " << SS_x << endl;
-        }
-        while (S_x != m)
-        {
-            S_x++;
-            cout << S_y << " " << S_x << endl;
-        }
-        
-        while(++S_y < n)
-        {
-            while (S_x != 0)
-            {
-                cout << S_y << " " << S_x << endl;
-                S_x--;
-            }
-            if(++S_y < 5)
-                break;
-            while (++S_x < m)
-            {
-                cout << S_y << " " << S_x << endl;
-
-            }
-            cout << S_y << " " << S_x << endl;
-        }
-        S_y = 0;
-        while (S_y++ != SS_y)
-        {
-            int bomb = 0;
-            while (S_x > 0)
-            {
-                cout << S_y << " " << S_x << endl;
-                S_x--;
-                bomb++;
-            }
-            if(bomb != 0)
-                continue;
-
-            while (S_x != m)
-            {
-                S_x++;
-                cout << S_y << " " << S_x << endl;      
-            }
-        }
-        
-        
-    
+    cout << "slm" << endl;
     return 0;
     
 }
