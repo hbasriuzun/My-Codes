accd5807b3f6c53b938b8d1623457a9f55b5bf27
hasan basri uzun <uzun.hbasri@gmail.com>
2023-07-05 01:59:06 +0300

    lk

---
+++ Compatitive Programming/Question solution/Codeforces/System_of_Equations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+
+#define endl "\n"
+const int N = 1e5+5;
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+
+    return 0;
+}
