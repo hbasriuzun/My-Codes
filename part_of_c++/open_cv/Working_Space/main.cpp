87987150db08d3a2a2707fe94d99f640c5e3f11e
hasan basri uzun <uzun.hbasri@gmail.com>
2023-07-07 23:36:38 +0300

    open cv learing

--- part_of_c++/open_cv/Working_Space/main.cpp
+++ part_of_c++/open_cv/Working_Space/main.cpp
@@ -10,14 +10,23 @@ using namespace std;
 
 int main()
 {
-    string path = "Resources/test.png";
+    string path = "Resources/Burcu.jpeg";
     Mat img = imread(path);
-    Mat imgGray;
-
+    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+    
     cvtColor(img, imgGray, COLOR_BGR2GRAY);
+    GaussianBlur(img, imgBlur, Size(5,5), 5, 0);
+    Canny(imgBlur, imgCanny, 50, 150);
+
+      
+    dilate(imgCanny, imgDil, getStructuringElement(MORPH_RECT, Size(5,5)));
 
     imshow("Image", img);
     imshow("Image Gray", imgGray);
+    imshow("Image Blur", imgBlur);
+    imshow("Image Canny", imgCanny);
+    imshow("Image Dilate", imgDil);
+
     waitKey(0);
 
     return 0;
