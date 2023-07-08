679a998ec01111df045821f9277cd1a10edf281d
hasan basri uzun <uzun.hbasri@gmail.com>
2023-07-08 11:20:36 +0300

    Open cv basic function

--- part_of_c++/open_cv/Working_Space/main.cpp
+++ part_of_c++/open_cv/Working_Space/main.cpp
@@ -4,28 +4,23 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+#define endl "\n"
 using namespace cv;
 using namespace std;
 
+/////////////// Basic Functions ///////////////
 
 int main()
 {
     string path = "Resources/Burcu.jpeg";
-    Mat img = imread(path);
-    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+    Mat img = imread(path), imgResize, imgCrop;
     
-    cvtColor(img, imgGray, COLOR_BGR2GRAY);
-    GaussianBlur(img, imgBlur, Size(5,5), 5, 0);
-    Canny(imgBlur, imgCanny, 50, 150);
+    cout << img.size() << endl;
 
-      
-    dilate(imgCanny, imgDil, getStructuringElement(MORPH_RECT, Size(5,5)));
-
+    resize(img, imgResize, Size(), 0.5, 0.5);
+    
     imshow("Image", img);
-    imshow("Image Gray", imgGray);
-    imshow("Image Blur", imgBlur);
-    imshow("Image Canny", imgCanny);
-    imshow("Image Dilate", imgDil);
+
 
     waitKey(0);
 
