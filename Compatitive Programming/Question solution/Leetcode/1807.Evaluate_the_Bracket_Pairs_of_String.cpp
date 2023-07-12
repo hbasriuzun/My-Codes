558969c75960ce0bd5ee27da52597374ce0e41db
hasan basri uzun <uzun.hbasri@gmail.com>
2023-07-12 16:11:20 +0300

    slm

--- Compatitive Programming/Question solution/Leetcode/1807.Evaluate_the_Bracket_Pairs_of_String.cpp
+++ Compatitive Programming/Question solution/Leetcode/1807.Evaluate_the_Bracket_Pairs_of_String.cpp
@@ -6,11 +6,18 @@ const int N = 1e5+5;
 using namespace std;
 typedef unsigned long long ull;
 typedef long long ll;
+typedef push_back pb;
+
+string evaluate(string s, vector<vector<string>>& knowledge) {
+    
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    string s = "(name)is(age)yearsold";
+    vector<vector<string>> knowledge = {{"name","bob"},{"age","two"}};
 
 
     return 0;
